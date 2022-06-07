6dbad6ecf02ab5c49705d5b9df4b9541d492f344
Ange1D <47231898+Ange1D@users.noreply.github.com>
2022-06-06 22:13:19 -0500

    creacion de pagina id

---
+++ pages/[id].tsx
@@ -0,0 +1,7 @@
+import type {NextPage} from "next";
+
+const id: NextPage = () => {
+  return <div>Hello id</div>;
+};
+
+export default id;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type {NextPage} from "next";
 
-const Home: NextPage = () => {
-  return <div>Hello future</div>;
+const HomePage: NextPage = () => {
+  return <div>Hello index</div>;
 };
 
-export default Home;
+export default HomePage;
